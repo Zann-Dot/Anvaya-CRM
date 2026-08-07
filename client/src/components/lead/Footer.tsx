82da283fd868303051a37708dfe2928f471e4349
Zann-Dot <anaykarn234@gmail.com>
2026-08-07 19:26:46 +0530

    client: feat: added disbaled css property to prev and next buttons

--- client/src/components/lead/Footer.tsx
+++ client/src/components/lead/Footer.tsx
@@ -8,12 +8,18 @@ interface FooterProps {
     setPage: Dispatch<SetStateAction<number>>;
     isPlaceholderData: boolean;
 }
+
+const DISABLED = "disabled:opacity-50 disabled:hover:text-gray-400 disabled:dark:hover:text-gray-500 disabled:dark:hover:bg-gray-800 disabled:cursor-default"
+
 export default function Footer({
     data,
     page,
     setPage,
     isPlaceholderData,
 }: FooterProps) {
+
+    console.log(isPlaceholderData);
+
     return (
         <div className="flex flex-col gap-3 border-t border-gray-100 px-4 py-3 sm:flex-row sm:items-center sm:justify-between dark:border-gray-700">
             <p className="text-xs text-gray-500 dark:text-gray-400">
@@ -32,7 +38,7 @@ export default function Footer({
                 <button
                     onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
                     disabled={page === 1}
-                    className="inline-flex cursor-pointer items-center gap-1 rounded-lg border border-gray-200 bg-white px-3 py-1.5 text-xs font-medium text-gray-400 transition-colors duration-200 ease-in-out hover:bg-gray-50 hover:text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-900/20 dark:hover:text-gray-400"
+                    className={`${DISABLED} inline-flex cursor-pointer items-center gap-1 rounded-lg border border-gray-200 bg-white px-3 py-1.5 text-xs font-medium text-gray-400 transition-colors duration-200 ease-in-out hover:bg-gray-50 hover:text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-900/20 dark:hover:text-gray-400`}
                 >
                     <HiOutlineChevronLeft className="h-3.5 w-3.5" />
                     Prev
@@ -45,8 +51,8 @@ export default function Footer({
                         if (data && !isPlaceholderData && page < data?.totalPages)
                             setPage((prev) => prev + 1);
                     }}
-                    disabled={data && isPlaceholderData && page >= data?.totalPages}
-                    className="inline-flex cursor-pointer items-center gap-1 rounded-lg border border-gray-200 bg-white px-3 py-1.5 text-xs font-medium text-gray-400 transition-colors duration-200 ease-in-out hover:bg-gray-50 hover:text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-900/20 dark:hover:text-gray-400"
+                    disabled={isPlaceholderData || (data && page >= data?.totalPages)}
+                    className={`${DISABLED} inline-flex cursor-pointer items-center gap-1 rounded-lg border border-gray-200 bg-white px-3 py-1.5 text-xs font-medium text-gray-400 transition-colors duration-200 ease-in-out hover:bg-gray-50 hover:text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-900/20 dark:hover:text-gray-400`}
                 >
                     Next
                     <HiOutlineChevronRight className="h-3.5 w-3.5" />
